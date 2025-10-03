4e73ae5b0421bdc33f7a1ae6c8356c997273121b
alekseiambarcumov <alekseiambarcumov@gmail.com>
2025-10-03 20:47:48 +0000

    Fix: Add missing React import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import useStore from './useStore';
 import Map from './components/Map';
